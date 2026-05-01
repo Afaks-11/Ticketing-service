d0ca7f8258060356d357615b51c0180a58bb3f8f
Hamman Wadzani Afakirya <afawadzani68@gmail.com>
2026-05-02 00:02:20 +0530

    refactor: node ES error and updated the PrismaClient import from @prisma/client. I also implemented the register route for  adding a new user

--- services/auth-service/src/auth/auth.service.ts
+++ services/auth-service/src/auth/auth.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+
+import { PrismaService } from '../prisma/prisma.service';
+import { RegisterDto } from './dto/register.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  async register(registerDto: RegisterDto) {
+    const { email, password } = registerDto;
+
+    const existingUser = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (existingUser) {
+      throw new ConflictException('A user with this email already exists');
+    }
+
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(password, saltRounds);
+
+    const newUser = await this.prisma.user.create({
+      data: {
+        email,
+        passwordHash,
+      },
+    });
+
+    return {
+      id: newUser.id,
+      email: newUser.email,
+      createdAt: newUser.createdAt,
+    };
+  }
+}
